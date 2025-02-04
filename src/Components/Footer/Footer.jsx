1ffc5a473c90381c0396dcdc4e809842e5b1ecdb
Autornas <ArnasLTUggLOL@gmail.com>
2025-02-04 14:28:49 +0200

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import style from './Footer.module.css';
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
 
 function Footer() {
-    return(
+    return (
         <div className={style.footer}>
             <p>© 2025 All rights reserved.</p>
+            <div className={style.socialIcons}>
+                <a href="https://www.instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram size={24} />
+                </a>
+                <a href="https://www.facebook.com/yourusername" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook size={24} />
+                </a>
+            </div>
         </div>
-
     );
 }
-
-export default Footer;
